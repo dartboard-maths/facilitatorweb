b64b8531708b2f95c444d97fdcb1abfb93182b0d
dartboard-maths <dbm@kineticdragon.co.za>
2026-05-24 20:28:20 +0200

    tutor footer

--- components/cms/CmsSiteChrome.tsx
+++ components/cms/CmsSiteChrome.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import type { CmsFooter, CmsHeader, CmsLink, CmsRenderContext } from "../../lib/cms/contentful";
 import { toParagraphs } from "../../lib/cms/contentful";
